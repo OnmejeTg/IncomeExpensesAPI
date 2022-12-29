f3f47649eca27cf0b58ad9b2ed471833be2b0e58
tgod <tonmeje@gmail.com>
2022-12-29 15:53:21 +0100

    Password reset

--- incomeexpensesapi/authentication/urls.py
+++ incomeexpensesapi/authentication/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, VerifyEmail,  LoginAPIView
+from .views import RegisterView, VerifyEmail,  LoginAPIView, SetNewPasswordAPIView, PasswordTokenCheckAPI, RequestPasswordResetEmail
 from rest_framework_simplejwt.views import (TokenRefreshView)
 
 urlpatterns = [
@@ -8,5 +8,9 @@ urlpatterns = [
     path('email-verify/', VerifyEmail.as_view(), name='email-verify'),
 
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('password-reset/<uidb64>/<token>/', PasswordTokenCheckAPI.as_view(), name='password-reset-confirm'),
+    path('request-reset-email/', RequestPasswordResetEmail.as_view(), name='request-reset-email'),
+    path('password-reset-complete', SetNewPasswordAPIView.as_view(), name='password-reset-complete'),
 
 ]
